b9bc340415bcd5c7413e3b49564aa3bedf3e31a7
danielkozmacs <daniel.kozmacs@zooshgroup.com>
2020-05-30 11:52:33 +0200

    Use heroku backend for Bgio server

--- components/App/Helpers/AddressHelper.ts
+++ components/App/Helpers/AddressHelper.ts
@@ -3,8 +3,10 @@ import SSRHelper from './SSRHelper';
 export default class AddressHelper {
   /** Gets bgio host:port address. */
   public static getServerAddress() {
+    const backendUrl = 'https://katicabackend.herokuapp.com';
     if (!SSRHelper.isSSR()) {
-      return process.env.BGIO_SERVER_URL || `http://${window.location.hostname}:8001`;
+      // return process.env.BGIO_SERVER_URL || `http://${window.location.hostname}:8001`;
+      return backendUrl;
     }
   }
 }
